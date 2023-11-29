866f2d1a85dc015ee43bbfd9dbdc2711a75c3590
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-29 18:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const table = document.querySelector('table');
+const newRow = document.createElement('tr');
+const newCol = document.createElement('td');
 const getSum = () => {
 //Add your code here
-  
+	const allPrice = document.querySelectorAll('.price');
+	let totalPrice = 0;
+
+	for(let price of allPrice){
+		totalPrice += parseInt(price.textContent);
+	}
+
+	newCol.textContent = `Total Price: Rs ${totalPrice}`;
+	
+	newRow.appendChild(newCol);
+	table.appendChild(newRow);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
